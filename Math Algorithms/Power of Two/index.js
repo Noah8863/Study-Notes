d7f85fbe764f6a2f38c3dbc346ae0c7feda94d18
Noah8863 <stacknoah99@gmail.com>
2023-09-12 10:20:49 -0600

    getting basic idea of Power of Two

---
+++ Math Algorithms/Power of Two/index.js
@@ -0,0 +1,7 @@
+//An integer is a power of two if there exists an integer 'x' such that 'n' === 2x
+
+//Problem: Give a positive integer 'n', determine if the number is a power of 2 or not. 
+
+
+isPowerOfTwo(1) = true(2^0)
+isPowerOfTwo(2) = true(2^1)

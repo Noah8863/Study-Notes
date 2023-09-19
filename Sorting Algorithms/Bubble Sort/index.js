6f8d8874ad00075b9cd78821c33bba44f3565369
Noah8863 <stacknoah99@gmail.com>
2023-09-18 21:34:50 -0600

    started bubble sort lesson

---
+++ Sorting Algorithms/Bubble Sort/index.js
@@ -0,0 +1,13 @@
+//Problem - Given an array of integers, sort the array. 
+
+//Bubble sort is not the best sorting algorithm in the real world!
+
+const arr = [-6, 20, 8, -2, 4]
+
+// bubbleSort(arr) -> Should return [-6, -2, 4, 8, 20]
+
+//Overall Idea;
+
+//Compare adjacent elements in the array and swap the positions if they are not in the intended order. This
+//Repeat the instruction as you step through each element in the array 
+//Once you step through the whole array with no swaps, the array is sorted. 
---
+++ Sorting Algorithms/index.js
@@ -0,0 +1,8 @@
+//Sorting is usually developed to show the newest item first or the item with the most number of upvotes in real world applications
+
+//The 4 more sorting algorithms covered are;
+
+//Bubble Sort
+//Insertion sort
+//Quick sort
+//Merge sort
